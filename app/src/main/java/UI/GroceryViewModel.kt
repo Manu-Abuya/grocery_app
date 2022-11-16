41e6421041a65f15a8dcedfb192aa59c122bbaad
Manu-Abuya <manuabuya69@gmail.com>
2022-11-16 10:21:25 +0300

    Grocery ViewModel

---
+++ app/src/main/java/UI/GroceryViewModel.kt
@@ -0,0 +1,23 @@
+package UI
+
+import androidx.lifecycle.ViewModel
+import com.example.groceryapp.Database.Entity.GroceryItems
+import com.example.grocerylist.Database.GroceryRepository
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class GroceryViewModel(private val repository: GroceryRepository) : ViewModel() {
+
+    // In the coroutines thread insert item in insert function
+    fun insert(item: GroceryItems) = GlobalScope.launch {
+        repository.insert(item)
+    }
+
+    // In coroutines thread delete item in delete function
+    fun delete(item: GroceryItems) = GlobalScope.launch {
+        repository.delete(item)
+    }
+
+    // We initialize allGroceryItems function with repository
+    fun allGroceryItems() = repository.allGroceryItems()
+}
